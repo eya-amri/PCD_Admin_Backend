7d89b05a647dfe4ede7a99a6ad02c5ebb530b25e
eya amri <eya.amri@ensi-uma.tn>
2025-05-08 13:07:27 +0100

    eya second commit

--- controller/admin.controller.js
+++ controller/admin.controller.js
@@ -46,6 +46,8 @@ const loginAdmin = async (req, res) => {
         status: 'error',
         message: 'Mot de passe incorrect',
       });
+    }else{
+      console.log('Authentification réussie');
     }
 
     // 4. Authentification réussie, on crée une session pour l'admin
@@ -58,10 +60,8 @@ const loginAdmin = async (req, res) => {
     // 5. Répondre avec succès
     return res.status(200).json({
       status: 'success',
-      message: 'Connexion réussie',
-      adminId: admin._id,
+      message: 'Authentification réussie'
     });
-
   } catch (err) {
     console.error('Erreur lors de la connexion de l\'admin:', err);
     // Gérer les erreurs serveur
